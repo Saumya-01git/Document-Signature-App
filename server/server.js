75423038ecf88d255bc91ba6159f14c183713236
Saumya <saumyagarg55555@gmail.com>
2026-06-06 23:21:44 +0530

    Day 2 Done: Implement JWT authentication and authorization

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./config/db");
+const authMiddleware = require("./middleware/authMiddleware");
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed successfully",
+    user: req.user,
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Document Signature App Backend Running");
