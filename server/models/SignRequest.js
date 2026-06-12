aea298f99ad56ec020517ad6bd0bd39d3ae07d98
Saumya <saumyagarg55555@gmail.com>
2026-06-12 23:19:57 +0530

    Day 8: Add sign request model

---
+++ server/models/SignRequest.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const signRequestSchema = new mongoose.Schema(
+  {
+    documentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Document",
+      required: true,
+    },
+
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    signerEmail: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["Pending", "Signed"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("SignRequest", signRequestSchema);
