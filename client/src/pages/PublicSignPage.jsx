9754913aa990812c87c97797942686dbac31fbbc
Saumya <saumyagarg55555@gmail.com>
2026-06-15 19:52:06 +0530

    Day 10: Read signing token from public route

--- client/src/pages/PublicSignPage.jsx
+++ client/src/pages/PublicSignPage.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 function PublicSignPage() {
+  const { token } = useParams();
+
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold">
@@ -7,6 +11,10 @@ function PublicSignPage() {
 
       <p className="mt-3">
         Public signing page
+      </p>
+
+      <p className="mt-3 text-sm text-gray-600">
+        Token: {token}
       </p>
     </div>
   );
