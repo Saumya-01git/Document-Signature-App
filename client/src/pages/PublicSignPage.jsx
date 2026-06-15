410f6fe16aaef92ddc00f99123c7e4877db1d1bd
Saumya <saumyagarg55555@gmail.com>
2026-06-15 19:44:47 +0530

    Day 10: Create public signer page

---
+++ client/src/pages/PublicSignPage.jsx
@@ -0,0 +1,15 @@
+function PublicSignPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">
+        Sign Document
+      </h1>
+
+      <p className="mt-3">
+        Public signing page
+      </p>
+    </div>
+  );
+}
+
+export default PublicSignPage;
